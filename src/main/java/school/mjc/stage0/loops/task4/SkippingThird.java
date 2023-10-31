b7b8d635db86ea20517b660f40eed043d28f6d82
yoqub-dev <yoqub.epam@gmail.com>
2023-10-31 15:19:29 +0500

    task13

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -3,5 +3,13 @@ package school.mjc.stage0.loops.task4;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
 
+        int count = 0;
+        for (int i = 1; i <= lastPrinted; i++) {
+            count++;
+            if (count % 3 != 0) {
+                System.out.println(i);
+            }
+        }
+
     }
 }
